961487dcbd3af2575e7939ef8b96a84fa3e796a3
rivanildojr <rivanildo_junior7@hotmail.com>
2023-06-04 23:35:00 -0300

    feat[SessionOne]: created about session

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import SessionOne from '@/components/SessionOne/index.vue'
+import AboutUs from '@/components/AboutUs/index.vue'
 </script>
 
 <template>
   <main>
     <SessionOne />
+    <AboutUs />
   </main>
 </template>
